eecbfefc92ce1a35f1376acee22485f740c0dca6
verbiricha <bandarra+gith@protonmail.com>
2023-12-18 12:52:25 +0100

    fix: qr code rendering

--- packages/core/src/components/QrCode.tsx
+++ packages/core/src/components/QrCode.tsx
@@ -1,16 +1,9 @@
-import QR from "react-qr-code";
+import { QRCodeSVG } from "qrcode.react";
 
 export interface QrCodeProps {
   data: string;
 }
 
-export default function QrCode(props: QrCodeProps) {
-  return (
-    <QR
-      size={256}
-      value={props.data}
-      style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-      viewBox={`0 0 256 256`}
-    />
-  );
+export default function QrCode({ data }: QrCodeProps) {
+  return <QRCodeSVG value={data} size={256} marginSize={8} />;
 }
